d8d52bd389e26bdcd7a5ae4c7a10905c2bc5302f
Antony Gelberg <antony.gelberg@gmail.com>
2022-08-26 15:32:54 +0300

    Implement pomodoro / break cycle. Fixes #1
    * Time class becomes Timer, which can have a type of break or pomodoro
    * Some function and variable renaming for clarity

--- pyredfruit.py
+++ pyredfruit.py
@@ -9,8 +9,9 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import GLib, Gtk
 
 
-class Time:
-    def __init__(self):
+class Timer:
+    def __init__(self, _type, minutes, seconds):
+        self.type = _type
         # We need to use datetime.datetime, not datetime.time.
         # See:
         #     https://bugs.python.org/issue1118748
@@ -22,17 +23,22 @@ class Time:
         # implement time tracking in the future, which would require
         # dates.
         today = datetime.date.today()
-        time = datetime.time(0, 25, 0)
-        # Five seconds for testing ;)
-        # time = datetime.time(0, 0, 5)
+        time = datetime.time(0, minutes, seconds)
         self.remaining = datetime.datetime.combine(today, time)
 
     def decrement_second(self):
         a_second = datetime.timedelta(seconds=1)
         self.remaining = self.remaining - a_second
 
-    def is_zero(self):
-        return bool(self.remaining.time() == datetime.time(0,0,0))
+    def done_text(self):
+        match self.type:
+            case "break":
+                return " Break over, time to focus"
+            case "pomodoro":
+                return "Timer up, take a break"
+
+    def has_expired(self):
+        return bool(self.remaining.time() == datetime.time(0, 0, 0))
 
     def __str__(self):
         return self.remaining.strftime("%M:%S")
@@ -42,46 +48,58 @@ class MainWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title=sys.argv[0])
 
-        self.time_left = Time()
+        self.timer = Timer("pomodoro", 25, 0)
 
         box = Gtk.Box(spacing=6)
         self.add(box)
 
         self.time_label = Gtk.Label()
         self.time_label.set_markup(
-            f'<span size="500%" weight="bold">{self.time_left}</span>'
+            f'<span size="500%" weight="bold">{self.timer}</span>'
         )
         box.pack_start(self.time_label, True, True, 0)
 
         button = Gtk.Button.new_with_label("Start")
-        button.connect("clicked", self.button_clicked)
+        button.connect("clicked", self.clicked_start)
         box.pack_start(button, True, True, 0)
 
-    def button_clicked(self, button):
+    def clicked_start(self, button):
         button.set_label("Pause")
-        self.start_timer()
+        self.start_pomodoro()
 
     def time_up(self):
         dialog = Gtk.MessageDialog(
-            buttons=Gtk.ButtonsType.OK,
-            text="Time up, take a break"
+            buttons=Gtk.ButtonsType.OK, text=self.timer.done_text()
         )
         dialog.run()
+        match self.timer.type:
+            case "break":
+                self.start_pomodoro()
+            case "pomodoro":
+                self.start_break()
         dialog.destroy()
 
+    def start_break(self):
+        self.timer = Timer("break", 5, 0)
+        self.start_tick()
+
+    def start_pomodoro(self):
+        self.timer = Timer("pomodoro", 25, 0)
+        self.start_tick()
+
+    def start_tick(self):
+        GLib.timeout_add_seconds(1, self.update_time)
+
     def update_time(self):
-        self.time_left.decrement_second()
+        self.timer.decrement_second()
         self.time_label.set_markup(
-            f'<span size="500%" weight="bold">{self.time_left}</span>'
+            f'<span size="500%" weight="bold">{self.timer}</span>'
         )
-        if self.time_left.is_zero():
+        if self.timer.has_expired():
             self.time_up()
             return False
 
         return True  # timeout_add_seconds will keep going
-
-    def start_timer(self):
-        GLib.timeout_add_seconds(1, self.update_time)
 
 
 if __name__ == "__main__":
